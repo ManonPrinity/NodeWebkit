29207b7e3d0783606cd023eb8cb719c5f7141654
noblet_m <manon.noblet@epitech.eu>
2016-04-01 09:50:07 +0200

    index+css+script

---
+++ New_WebKit/script/app.js
@@ -0,0 +1,21 @@
+window.ondragover = function(e) { e.preventDefault; return false;}
+window.ondrop = function(e) { e.preventDefault; return false;}
+
+var el = document.querySelector('#drop');
+el.ondragover = function () {
+	this.className = "hover";
+	this.innerHTML = "Drop the file";
+	return false;
+}
+el.ondragleave = function () {
+	this.className = "";
+	this.innerHTML = "Drop your icon here ^^";
+	return false;
+}
+el.ondrop = function (e) {
+	e.preventDefault();
+	console.log(e.dataTransfer);
+	for (var i =0; i < e.dataTransfer.file.lenght; ++i) {
+
+	}
+}
